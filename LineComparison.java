7d2f2a91b7fb45dc98e2bf645ad96fb3ceabb4ca
Bharath3222 <bharat322.korada@gmail.com>
2022-07-03 09:51:05 +0530

    Welcome to Line comparison problem

--- LineComparison.java
+++ LineComparison.java
@@ -19,27 +19,6 @@ public class LineComparison {
 
 		System.out.println("Length of Line 1 is : " + LengthOfLine);
 		
-		System.out.println("Enter second line coordinates : ");
-		int a1 = scanner.nextInt();
-		int a2 = scanner.nextInt();
-		int b1 = scanner.nextInt();
-		int b2 = scanner.nextInt();
-		System.out.println(
-				"entered coordinates for line1 are : " + "(" + a1 + "," + b1 + ") and (" + a2 + "," + b2 + ")");
-		int Line2 = ((a2 - a1) * (a2 - a1)) + ((b2 - b1) * (b2 - b1));
-		double LengthOfLine_2 = Math.sqrt(Line2);
-
-		System.out.println("Length of Line 2 is : " + LengthOfLine_2);
-		// uc2
-		if (LengthOfLine == LengthOfLine_2)
-			System.out.println("Both lines are equal");
-		else {
-			System.out.println("Both lines are not equal");
-			if (LengthOfLine < LengthOfLine_2) {
-				System.out.println("Line 2 is greater");
-			} else {
-				System.out.println("Line 1 is greater");
-			}}
 	}
 
 }
